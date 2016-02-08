7003da4788abbee4813fa29fd37474228f5b5180
f.carmagnola <f.carmagnola@reply.it>
2016-02-08 16:16:36 +0100

    Added a method to aggregate horizontal lines

--- main.js
+++ main.js
@@ -13,20 +13,35 @@ if(pathPicture != null){
   buffer =  fs.readFileSync(pathPicture, 'utf8');
 }
 var pictureFile = buffer.split('\n');
-picRows = pictureFile[0].split(' ')[0];
-picCols = pictureFile[0].split(' ')[1];
 
 //clone
 picture = pictureFile.slice(0);
 
 //eliminate first row
 picture.splice(0,1);
+//eliminate last row (is blank!)
+picture.pop();
+
+var picRows = picture.length;
+var picCols = picture[0].length;
+
+
 //now, picture is a perfect matrix of points. nj element = picture[n][j]
 
+var hArray = createHorizontalArray();
 
-createTrivialSolution();
+for (var i in hArray) {
+  if(hArray[i]){
+    var elem = hArray[i];
+    pushPaintLine(elem[0],elem[1],elem[2],elem[3]);
+  }
+}
+
+outputSolution();
 
 
+
+/* ----------------------- FUNCTIONS --------------------------------------------- */
 
 function pushPaintSquare(r,c,s) {
   solution.push('PAINT_SQUARE '+r+' '+c+' '+s);
@@ -62,4 +77,36 @@ function createTrivialSolution(){
     }
   }
   outputSolution();
+}
+
+
+function createHorizontalArray(){
+
+  var hArray = [];
+
+  for (var r = 0; r < picRows; r++) {
+    var startPoint = null;
+    var endPoint = null;
+    for(var c = 0; c < picCols; c++){
+      if(picture[r][c] === full && c != picCols-1){
+        if(startPoint){
+          if(endPoint <= c){
+            endPoint = c;
+          }
+        } else {
+          startPoint = c;
+        }
+      } else if ( (picture[r][c] !== full && startPoint && endPoint) ||
+                  (startPoint && picture[r][c] === full && c == picCols-1)
+      ){
+        if(!endPoint){
+          endPoint = c;
+        }
+        hArray.push([r,startPoint,r,endPoint]);
+        startPoint = null;
+        endPoint = null;
+      }
+    }
+  }
+  return hArray;
 }
